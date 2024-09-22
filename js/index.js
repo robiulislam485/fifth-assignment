e7ddb2b04bf75163cb6251809bee7a160e13e9b6
robiulislam485 <jibonmahmudrobi485@gmail.com>
2024-09-23 01:38:53 +0600

    added design of donate page

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('first-donate-button').addEventListener('click', function(){
+    const firstInputValue = getValueFromInput('first-donate-input');
+    console.log(firstInputValue);
+})
---
+++ js/utilities.js
@@ -0,0 +1,5 @@
+function getValueFromInput(id){
+   const inputValue = document.getElementById(id);
+   const inputNumber = parseFloat(inputValue);
+   return inputNumber;
+}
