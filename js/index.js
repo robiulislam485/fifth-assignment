ae909fa021c831d2806643ed8ec5bd6a86ecca9d
robiulislam485 <jibonmahmudrobi485@gmail.com>
2024-09-23 02:25:08 +0600

    first card interact done by DOM

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 document.getElementById('first-donate-button').addEventListener('click', function(){
     const firstInputValue = getValueFromInput('first-donate-input');
-    console.log(firstInputValue);
-})
+    const currentAmount = document.getElementById('first-donate-current-amount').innerText;
+    const finalAmount = firstInputValue + parseFloat(currentAmount);
+    if( firstInputValue > 0 && typeof(firstInputValue) === 'number'){
+        document.getElementById('first-donate-current-amount').innerText = finalAmount;
+    }
+    else{
+        alert('Invalid Amount!')
+    }
+});
--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,5 @@
 function getValueFromInput(id){
-   const inputValue = document.getElementById(id);
-   const inputNumber = parseFloat(inputValue);
-   return inputNumber;
-}
+    const getInputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(getInputValue);
+    return inputNumber;
+};
