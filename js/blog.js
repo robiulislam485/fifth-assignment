6e3f0761fd2aa3b9af3ca1ed3a893f2a99ba8a50
robiulislam485 <jibonmahmudrobi485@gmail.com>
2024-09-23 12:57:55 +0600

    added history and donation button its done

---
+++ js/blog.js
@@ -0,0 +1,4 @@
+const goBack = document.getElementById('home-button');
+goBack.addEventListener('click', function(){
+   window.location.href = '/index.html'
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+const blogButton = document.getElementById('blog-button');
+blogButton.addEventListener('click', function(){
+    window.location.href = '/blog.html';
+});
+
+
+
 document.getElementById('first-donate-button').addEventListener('click', function(){
     const firstInputValue = getValueFromInput('first-donate-input');
     const currentAmount = document.getElementById('first-donate-current-amount').innerText;
@@ -9,3 +16,18 @@ document.getElementById('first-donate-button').addEventListener('click', functio
         alert('Invalid Amount!')
     }
 });
+
+const donationTab = document.getElementById('doanation-button');
+const historyTab = document.getElementById('history-button');
+historyTab.addEventListener('click', function(){
+    
+    donationTab.classList.remove('bg-buttonColor');
+    historyTab.classList.remove('bg-coinColor');
+    historyTab.classList.add('bg-buttonColor');
+    document.getElementById('card-section').classList.add('hidden');
+});
+donationTab.addEventListener('click', function(){
+    historyTab.classList.remove('bg-buttonColor');
+    donationTab.classList.add('bg-buttonColor');
+    document.getElementById('card-section').classList.remove('hidden');
+})
